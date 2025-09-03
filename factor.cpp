b73244eed4302fd17d09196c3755fc6167d3f9b7
shivani <shivanibisen55@gmail.com>
2025-09-03 12:26:00 +0530

    modify

--- factor.cpp
+++ factor.cpp
@@ -1,16 +1,20 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
-int main()
-{
-    int n;
-    cout<<"enter number";
-    cin>>n;
-    if(int i=1;i<=n; ++i)
-    {
-        if(n%1==0)
-        {
-            cout<<i;
-        }
 
+int main() {
+    int num;
+
+    cout << "Enter a number: ";
+    cin >> num;
+
+    cout << "Factors of " << num << " are: ";
+
+    for (int i = 1; i <= num; i++) {
+        if (num % i == 0) {
+            cout << i << " ";
+        }
     }
+
+    cout << endl;
+return 0;
 }
